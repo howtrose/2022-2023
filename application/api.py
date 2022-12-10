c5bad86e4d88ca7046adfe0a1edabf446837a003
howtrose <120164162+howtrose@users.noreply.github.com>
2022-12-10 15:47:48 -0600

    Add files via upload

--- application/api.py
+++ application/api.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 class UserAPI(Resource):
     def get(self, username):
         print("GET username", username)
-        return {"username" : username}
+        return {"username": username}
 
     def put(self, username):
         print("PUT username", username)
